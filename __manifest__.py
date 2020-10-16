85a6127c72a4510b6924e6eec2189e9bca26a735
jefferson <jeffer.leo93@gmail.com>
2020-10-16 01:08:59 -0600

    codigo de barras

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Quemen",
+
+    'summary': """ Desarrollo extra quemen """,
+
+    'description': """
+        Desarrollo extra pra quemen
+    """,
+
+    'author': "JS",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['stock','base'],
+
+    'data': [
+        'views/report.xml',
+        'views/reporte_codigo_barras.xml',
+    ],
+}
