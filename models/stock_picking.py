9d252afbd112f5da9fef45bc8a5507e8693c2272
jefferson <jeffer.leo93@gmail.com>
2020-10-26 12:32:32 -0600

    Permiso validar envio y reporte existencia

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -2,16 +2,49 @@
 from odoo import api, fields, models, SUPERUSER_ID, _
 from odoo.exceptions import UserError
 import logging
-
+import pytz
 
 class Picking(models.Model):
     _inherit = "stock.picking"
 
     def button_validate(self):
-        res = super(Picking, self).button_validate()
-        if self.env.user.has_group('quemen.quemen_validar_envio_tienda') and self.picking_type_id.code == 'internal':
-            return res
-        elif self.env.user.has_group('quemen.quemen_validar_envio_tienda') == False and self.picking_type_id.code != 'internal' :
-            return res
+        logging.warn('123456789')
+        if self.picking_type_id.code == 'internal':
+            if self.location_dest_id.id == self.env.user.pos_id.picking_type_id.default_location_src_id.id:
+                return super(Picking, self).button_validate()
+            else:
+                return UserError(_('No tiene permisos para validar'))
         else:
-            raise UserError(_('No tiene permisos para validar'))
+            return super(Picking, self).button_validate()
+        # if self.env.user.pos_id.picking_type_id
+        # if self.env.user.has_group('quemen.quemen_validar_envio_tienda') and self.picking_type_id.code == 'internal':
+        #     return res
+        # elif self.env.user.has_group('quemen.quemen_validar_envio_tienda') == False and self.picking_type_id.code != 'internal' :
+        #     return res
+        # else:
+        #     raise UserError(_('No tiene permisos para validar'))
+
+    def verificar_productos_vencidos(self):
+        logging.warn('verificar para albaran')
+        stock_quant = self.env['stock.quant'].search([('quantity','>',0)])
+        timezone = pytz.timezone(self._context.get('tz') or self.env.user.tz or 'UTC')
+        fecha_hoy = datetime.datetime.now().astimezone(timezone).strftime('%Y-%m-%d')
+        logging.warn(fecha_hoy)
+        inventario = {}
+        if stock_quant:
+            for linea in stock_quant:
+                if linea.location_id.id not in inventario:
+                    inventario[linea.location_id.id] = {'productos':[],'bodega':linea.location_id}
+
+                if linea.lot_id and linea.lot_id.life_date and linea.lot_id.life_date.strftime('%Y-%m-%d') == fecha_hoy:
+                    logging.warn(linea.lot_id.life_date)
+                    inventario[linea.location_id.id]['productos'].append(linea)
+
+        logging.warn(inventario)
+        logging.warn('termina')
+        return inventario
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    devolucion_productos_vencidos = fields.Boolean('¿Desea utilizar este tipo de albaran para devolucion productos vencidos automatico?')
--- report/reporte_existencias.py
+++ report/reporte_existencias.py
@@ -16,8 +16,61 @@ import pytz
 class ReportExistencias(models.AbstractModel):
     _name = 'report.quemen.reporte_existencias'
 
+
+    def verificar_productos_vencidos(self):
+        logging.warn('verificar para albaran')
+        stock_quant = self.env['stock.quant'].search([('quantity','>',0)])
+        timezone = pytz.timezone(self._context.get('tz') or self.env.user.tz or 'UTC')
+        fecha_hoy = datetime.datetime.now().astimezone(timezone).strftime('%Y-%m-%d')
+        logging.warn(fecha_hoy)
+        inventario = {}
+        if stock_quant:
+            for linea in stock_quant:
+                if linea.location_id.id not in inventario:
+                    inventario[linea.location_id.id] = {'productos':[],'ubicacion':linea.location_id}
+
+                if linea.lot_id and linea.lot_id.life_date and linea.lot_id.life_date.strftime('%Y-%m-%d') == fecha_hoy:
+                    logging.warn(linea.lot_id.life_date)
+                    inventario[linea.location_id.id]['productos'].append(linea)
+
+        logging.warn(inventario)
+        logging.warn('termina')
+        inventario = inventario.values()
+        for dato in inventario:
+            logging.warn(dato)
+            if len(dato['productos']) > 0:
+                ubicacion = dato['ubicacion']
+                tipo_albaran = self.env['stock.picking.type'].search([('default_location_src_id','=', ubicacion.id),('devolucion_productos_vencidos','=',True)])
+                if tipo_albaran:
+                    logging.warn('tipo albaran')
+                    logging.warn(tipo_albaran)
+                    moves = {}
+                    envio = {'picking_type_id': tipo_albaran.id,'location_id': tipo_albaran.default_location_src_id.id, 'location_dest_id': tipo_albaran.default_location_dest_id.id}
+                    picking_id = self.env['stock.picking'].create(envio)
+                    for linea in dato['productos']:
+                        mv = {'name': linea.product_id.name,'product_id': linea.product_id.id,
+                        'product_uom':linea.product_uom_id.id,
+                        'product_uom_qty': linea.quantity,'picking_id':picking_id.id,'location_id':tipo_albaran.default_location_src_id.id,'location_dest_id':tipo_albaran.default_location_dest_id.id}
+                        move_id = self.env['stock.move'].create(mv)
+                        # move_id.write({'move_line_ids':[(0, 0, {'move_id':move_id.id,'location_id': move_id.location_id.id, 'location_dest_id': move_id.location_dest_id.id,'lot_id':linea.lot_id.id,'qty_done': linea.quantity,'product_uom_id': linea.product_uom_id.id,'company_id':tipo_albaran.company_id.id})]})
+                        if move_id.id not in moves:
+                            moves[move_id.id] = {'move': move_id,'lot_id': linea.lot_id}
+                    picking_id.action_assign()
+
+                    # for linea in picking_id.
+
+                    # for m in moves.values():
+                    #     ml = {'move_id': m['move'].id,'location_id': m['move'].location_id.id, 'location_dest_id': m['move'].location_dest_id.id,'lot_id':m['lot_id'].id,'qty_done': m['move'].product_uom_qty,'product_uom_id': m['move'].product_uom.id,'company_id':tipo_albaran.company_id.id}
+                    #     move_line = self.env['stock.move.line'].create(ml)
+                        # if move_id:
+                        #     ml = {'move_id':move_id.id,'location_id': move_id.location_id.id, 'location_dest_id': move_id.location_dest_id.id,'lot_id':linea.lot_id.id,'qty_done': linea.quantity,'product_uom_id': linea.product_uom_id.id,'company_id':tipo_albaran.company_id.id}
+                        #     move_line = self.env['stock.move.line'].create(ml)
+
+        return inventario
+
     def productos_existencia(self):
         # logging.warn(fecha_vencimiento)
+        self.verificar_productos_vencidos()
         ubicacion_id = self.env.user.pos_id.picking_type_id.default_location_src_id
         stock_id = self.env['stock.quant'].search([('location_id','=',ubicacion_id.id)])
         inventario = {}
