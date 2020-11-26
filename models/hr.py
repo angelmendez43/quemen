9d41539fabad591e2ed4d9ccfdab5397adcef1a8
jefferson <jeffer.leo93@gmail.com>
2020-11-26 09:12:03 -0600

    Hora empleados

--- models/hr.py
+++ models/hr.py
@@ -6,3 +6,5 @@ class Employee(models.Model):
     _inherit = "hr.employee"
 
     id_reloj = fields.Char('ID reloj checador')
+    hora_entrada = fields.Float('Hora entrada')
+    hora_salida = fields.Float('Hora salida')
