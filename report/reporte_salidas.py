f0afdba7aebe514c62c0510cc65b7025391c8ffe
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2021-02-09 01:44:53 -0600

    Update reporte_salidas.py

--- report/reporte_salidas.py
+++ report/reporte_salidas.py
@@ -15,3 +15,15 @@ import pytz
 
 class ReportSalidas(models.AbstractModel):
     _name = 'report.quemen.reporte_salidas'
+
+    
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        model = self.env.context.get('active_model')
+        docs = self.env[model].browse(self.env.context.get('active_ids', []))
+        return {
+            'doc_ids': self.ids,
+            'doc_model': model,
+            'data': data['form'],
+            'docs': docs
+        }
