4e0b6f84c2a977a09b6917b5b95bcf2497585603
jeffrepo <jeffer.leo93@gmail.com>
2022-03-21 22:20:42 -0600

    cambios varios

--- models/__init__.py
+++ models/__init__.py
@@ -2,11 +2,11 @@
 
 from . import stock_picking
 from . import res_users
-from . import quemen
+# from . import quemen
 from . import hr
 from . import pos_config
-from . import account_bank_statement
+# from . import account_bank_statement
 from . import pos_session
-from . import pos_order
+# from . import pos_order
 from . import stock_quant
 from . import product_template_extra_fields
