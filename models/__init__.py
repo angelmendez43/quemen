cceaf2a0eb0c4f90500e57c8f2a99b08dbf78ce6
jeffrepo <jeffer.leo93@gmail.com>
2024-04-30 11:40:49 -0600

    update

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 from . import stock_picking
 from . import res_users
 from . import quemen
-from . import hr
+#from . import hr
 from . import pos_config
 # from . import account_bank_statement
 from . import pos_session
