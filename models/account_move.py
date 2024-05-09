89df78b0f9965f6f37afc784941a0eaedca20f9a
jeffrepo <jeffer.leo93@gmail.com>
2024-05-09 12:56:56 -0600

    update

--- models/account_move.py
+++ models/account_move.py
@@ -6,6 +6,34 @@ import logging
 import pytz
 from datetime import datetime
 
+class AccountMove(models.Model):
+    _inherit = "account.move"
+    
+    def generar_factura_nueva(self, factura):
+        for f in factura:
+            factura_dic = {
+                'partner_id': f.partner_id.id,
+                'ref': f.ref,
+                'invoice_date': f.invoice_date,
+                'invoice_origin': f.invoice_origin,
+                'journal_id': f.journal_id.id,
+                'l10n_mx_edi_usage': 'S01',
+                'move_type': 'out_invoice',
+                'pos_order_ids': [(6, 0, f.pos_order_ids.ids)] if f.pos_order_ids else [],
+                'invoice_line_ids': [(0, None, self._prepare_invoice_line(line)) for line in f.invoice_line_ids],
+            }
+            factura_id = self.env['account.move'].create(factura_dic)
+
+    def _prepare_invoice_line(self, order_line):
+        return {
+            'product_id': order_line.product_id.id,
+            'quantity': order_line.quantity,
+            'discount': order_line.discount,
+            'price_unit': order_line.price_unit,
+            'name': order_line.name,
+            'tax_ids': [(6, 0, order_line.product_id.taxes_id.ids)],
+            'product_uom_id': order_line.product_uom_id.id,
+        }
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
